1daceb24cb82847c9a2229ea9d3a8e92b34b2e69
rockkult06 <okucukali@outlook.com>
2025-06-26 21:50:47 +0300

    logot

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import Header from "@/components/Header"
+import Header from "@/components/header"
 import HeroSection from "@/components/HeroSection"
 import MainContent from "@/components/MainContent"
 import Footer from "@/components/Footer"
